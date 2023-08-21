1c6aec814148d43271b131f19523b43b2b2ee96c
monicaGY <monicagarciayadaicela@gmail.com>
2023-08-21 16:20:27 +0200

    Creando la entidad usuario

--- src/Entity/ProductoFoto.php
+++ src/Entity/ProductoFoto.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ProductoFotoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ProductoFotoRepository::class)]
 class ProductoFoto
