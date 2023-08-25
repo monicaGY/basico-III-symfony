12879fb31d029da9ca1ab6338c461ce5c5f1b1d0
monicaGY <monicagarciayadaicela@gmail.com>
2023-08-25 16:29:31 +0200

    Destruir sesiones

---
+++ src/EventListener/LogoutSubscriber.php
@@ -0,0 +1,47 @@
+<?php
+// src/EventListener/LogoutSubscriber.php
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class LogoutSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator,
+        private RequestStack $requestStack
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [LogoutEvent::class => 'onLogout'];
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        // get the security token of the session that is about to be logged out
+        $token = $event->getToken();
+
+        // get the current request
+        $request = $event->getRequest();
+
+        // get the current response, if it is already set by another listener
+        $response = $event->getResponse();
+
+        $session = $this->requestStack->getSession();
+        $session->remove('perfil_nombre');
+        // configure a custom logout response to the homepage
+        $response = new RedirectResponse(
+            $this->urlGenerator->generate('acceso_login'),
+            RedirectResponse::HTTP_SEE_OTHER
+        );
+        $event->setResponse($response);
+    }
+}
+?>
